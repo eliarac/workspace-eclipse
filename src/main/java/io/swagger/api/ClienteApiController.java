c7e8606f9e5264ecfd98b5e8e2a86c1722c5ed6b
Eliara Costa <eliara865@gmail.com>
2019-08-05 17:25:11 -0300

    Removendo Mock do Cadastro

--- src/main/java/io/swagger/api/ClienteApiController.java
+++ src/main/java/io/swagger/api/ClienteApiController.java
@@ -1,54 +1,41 @@
 package io.swagger.api;
 
-import io.swagger.model.Cliente;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.ApiParam;
+import io.swagger.model.Cliente;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-08-05T18:00:01.964Z")
 
 @Controller
 public class ClienteApiController implements ClienteApi {
-
-    private static final Logger log = LoggerFactory.getLogger(ClienteApiController.class);
-
-    private final ObjectMapper objectMapper;
-
-    private final HttpServletRequest request;
-
-    @org.springframework.beans.factory.annotation.Autowired
-    public ClienteApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private ResponsesUtil responsesUtil;
 
     public ResponseEntity<Cliente> cadastraCliente(@ApiParam(value = ""  )  @Valid @RequestBody Cliente cliente_) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Cliente>(objectMapper.readValue("{  \"senha\" : \"senha123\",  \"cpf\" : \"01234567891234\",  \"conta\" : [ {    \"transacao\" : [ {      \"tipo\" : \"debito\",      \"data\" : \"2000-01-23T04:56:07.000+00:00\",      \"valor\" : 6.027456183070403,      \"id\" : 0    }, {      \"tipo\" : \"debito\",      \"data\" : \"2000-01-23T04:56:07.000+00:00\",      \"valor\" : 6.027456183070403,      \"id\" : 0    } ],    \"numConta\" : \"0023148\",    \"saldo\" : 105.35,    \"agencia\" : \"2779\",    \"digito\" : 5  }, {    \"transacao\" : [ {      \"tipo\" : \"debito\",      \"data\" : \"2000-01-23T04:56:07.000+00:00\",      \"valor\" : 6.027456183070403,      \"id\" : 0    }, {      \"tipo\" : \"debito\",      \"data\" : \"2000-01-23T04:56:07.000+00:00\",      \"valor\" : 6.027456183070403,      \"id\" : 0    } ],    \"numConta\" : \"0023148\",    \"saldo\" : 105.35,    \"agencia\" : \"2779\",    \"digito\" : 5  } ],  \"id\" : 123,  \"titular\" : \"Eliara Costa\"}", Cliente.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Cliente>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<Cliente>(HttpStatus.NOT_IMPLEMENTED);
+        
+    	try {
+    		//Salvar o cliente
+    		return clienteService.save(cliente_);
+    	} catch(Exception e){
+    		return responsesUtil.getInternalErrrorClient(clienteService.FALHA_AO_TENTAR_CADASTRAR_UM_CLIENTE)
+    		//Resposta de erro
+    	}
     }
-
 }
